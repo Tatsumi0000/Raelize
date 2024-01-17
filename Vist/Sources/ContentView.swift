ef0e376b548c19dfd5ee920ccf1b4a8adde2c670
Tatsumi0000 <info@aespa.love>
2024-01-18 00:28:47 +0900

    add xcode projects

---
+++ Vist/Sources/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Vist
+//
+//  Created by Tatsumi0000 on 2024/01/17.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vist/Sources/VistApp.swift
@@ -0,0 +1,18 @@
+//
+//  VistApp.swift
+//  Vist
+//
+//  Created by Tatsumi0000 on 2024/01/17.
+//  
+//
+
+import SwiftUI
+
+@main
+struct VistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
