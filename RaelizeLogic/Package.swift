bb0caa67179fe4257576c4a6b9dff0f067b60dd6
Tatsumi0000 <info@aespa.love>
2024-01-19 01:18:30 +0900

    Add dependency library  llama

--- RaelizeLogic/Package.swift
+++ RaelizeLogic/Package.swift
@@ -11,11 +11,20 @@ let package = Package(
             name: "RaelizeLogic",
             targets: ["RaelizeLogic"]),
     ],
+    dependencies: [
+        .package(
+                    url: "https://github.com/ggerganov/llama.cpp",
+                    revision: "b1892"), // 2024-01-19 latest version
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RaelizeLogic"),
+            name: "RaelizeLogic",
+            dependencies: [
+                .product(name: "llama", package: "llama.cpp")
+            ],
+            path: "./Sources"),
         .testTarget(
             name: "RaelizeLogicTests",
             dependencies: ["RaelizeLogic"]),
