5d34bb32dce0c77bc5d17b9d8c53143c579a68ba
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-03-26 00:46:55 +0900

    Add file loading process

--- RaelizeLogic/Sources/RaelizeLogic/Repository/WordListFileRepository.swift
+++ RaelizeLogic/Sources/RaelizeLogic/Repository/WordListFileRepository.swift
@@ -6,15 +6,38 @@
 //
 
 import Foundation
+import Combine
 
 protocol WordListFileRepositoryType {
+    /// Read file. And, set word-list to wordListInFile.
+    /// - Parameter fileName: File name
     func readFile(fileName: String)
+    /// Get read file content
+    func getWordList() -> AnyPublisher<[String]?, Never>
+    /// Reset current file content.
+    func resetFile()
 }
 
-class WordListFileRepository: WordListFileRepositoryType {
+final class WordListFileRepository: WordListFileRepositoryType {
+    /// Current opened file content.
+    private let wordListInFile = CurrentValueSubject<[String]?, Never>(nil)
 
-    public func readFile(fileName: String) {
-        print(fileName)
+     func readFile(fileName: String) {
+        guard let fileUrl = Bundle.module.url(forResource: "WordList/" + fileName, withExtension: "tsv"),
+              let data = try? String(contentsOf: fileUrl) else {
+            self.wordListInFile.send(nil)
+                  return
+              }
+        self.wordListInFile.send(data.components(separatedBy: .newlines))
     }
-
+    
+    func getWordList() -> AnyPublisher<[String]?, Never> {
+        return Future { result in
+            result(.success(self.wordListInFile.value))
+        }.eraseToAnyPublisher()
+    }
+    
+     func resetFile() {
+        self.wordListInFile.send(nil)
+    }
 }
