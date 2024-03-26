ffce4e8429e17ed14d29d2bd9d00a5a5c85eb905
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-03-26 18:01:31 +0900

    Create deubg app
    IME app is very difficult to verify.

---
+++ RaelizeDebug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//
+//  Created by Tatsumi0000 on 2024/03/26
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RaelizeDebug/RaelizeDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaelizeDebugApp.swift
+//
+//  Created by Tatsumi0000 on 2024/03/26
+//
+//
+
+import SwiftUI
+
+@main
+struct RaelizeDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
