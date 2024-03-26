b8677587048752697cbe8d3ae8c508a5e03018bb
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-03-26 23:54:59 +0900

    Add debug code

--- RaelizeDebug/Sources/ContentView.swift
+++ RaelizeDebug/Sources/ContentView.swift
@@ -6,16 +6,28 @@
 //
 
 import SwiftUI
+import RaelizeLogic
+import Combine
 
 struct ContentView: View {
+    @State var inputText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            TextField("Input text", text: $inputText)
+        }.onAppear(perform: {
+            UseCaseProvider.shared.wordListFileUseCase.readFile(fileName: "a")
+            
+            let cancellables =  UseCaseProvider.shared.wordListFileUseCase.currentWordList
+                .sink(receiveValue: {
+                    print("------sink------")
+                    print($0)
+                })
+        })
         .padding()
+        Button("Button", action: {
+            UseCaseProvider.shared.wordListFileUseCase.searchWordList(word: inputText)
+        })
     }
 }
 
