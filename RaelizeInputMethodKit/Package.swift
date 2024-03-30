10e7edcb211c81f04201393d7c25326f25d62b06
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-03-30 23:55:26 +0900

    Add Actomaton library

--- RaelizeInputMethodKit/Package.swift
+++ RaelizeInputMethodKit/Package.swift
@@ -14,11 +14,17 @@ let package = Package(
             name: "RaelizeInputMethodKit",
             targets: ["RaelizeInputMethodKit"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/Actomaton/Actomaton", from: "0.8.0")
+      ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RaelizeInputMethodKit"),
+            name: "RaelizeInputMethodKit",
+            dependencies: ["Actomaton"]
+        ),
+        
         .testTarget(
             name: "RaelizeInputMethodKitTests",
             dependencies: ["RaelizeInputMethodKit"]),
