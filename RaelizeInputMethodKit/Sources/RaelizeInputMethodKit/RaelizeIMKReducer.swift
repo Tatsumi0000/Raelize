9e7d180bc95254705bd92347e3510eaac86e1c53
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-04-03 22:53:26 +0900

    Create reduce

--- RaelizeInputMethodKit/Sources/RaelizeInputMethodKit/RaelizeIMKReducer.swift
+++ RaelizeInputMethodKit/Sources/RaelizeInputMethodKit/RaelizeIMKReducer.swift
@@ -6,30 +6,36 @@
 //
 
 import AppKit
+import Combine
 import ComposableArchitecture
 import Foundation
+import RaelizeLogic
 
 @Reducer
 public struct RaelizeIMKReducer {
 
+    private let provider = UseCaseProvider.shared.wordListFileUseCase
+
     /// UI state
     @ObservableState
-    struct State: Equatable {
+    public struct State: Equatable {
         var isCandidatesShowing: Bool = false
-        var candinates: [String]? = nil
+        var candinates: [String] = []
         var inputWord: String? = nil
         var selectedWord: String? = nil
     }
 
     /// User action
-    enum Action {
+    public enum Action {
         /// Operation keys(Enter, Arrow and so on)
         case operationEventKey(NSEvent.SpecialKey)
         /// Typing word
         case inputWord(String?)
+        /// Searched word
+        case checkWord([String]?)
     }
 
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    public func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
         case .operationEventKey(.enter):
             return .none
@@ -39,6 +45,19 @@ public struct RaelizeIMKReducer {
             return .none
         case .inputWord(let word):
             print(word ?? "word is nil.")
+            state.inputWord = word
+            return .publisher({
+                provider.searchWordList(word: "")
+                    .map({ Action.checkWord($0) })
+            })
+        case .checkWord(let words):
+            guard let words = words else {
+                state.isCandidatesShowing = false
+                state.candinates = []
+                state.selectedWord = nil
+                return .none
+            }
+            state.candinates = words
             return .none
         default:
             return .none
