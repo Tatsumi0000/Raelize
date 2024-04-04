88b7bd71f68d30b6a0a13f21382fd04d0eaefe68
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-04-05 01:18:22 +0900

    Add local library

--- RaelizeInputMethodKit/Package.swift
+++ RaelizeInputMethodKit/Package.swift
@@ -9,34 +9,32 @@ let package = Package(
         .macOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "RaelizeInputMethodKit",
-            targets: ["RaelizeInputMethodKit"])
+            name: "RaelizeInputMethodKit", targets: ["RaelizeInputMethodKit"])
     ],
     dependencies: [
         .package(
-            url: "https://github.com/pointfreeco/swift-composable-architecture",
-            from: "1.9.2"
-        ),
+            url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.9.2"),
         .package(url: "https://github.com/apple/swift-testing.git", from: "0.6.0"),
+        .package(name: "RaelizeLogic", path: "../RaelizeLogic"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "RaelizeInputMethodKit",
             dependencies: [
+                "RaelizeLogic",
                 .product(
                     name: "ComposableArchitecture",
                     package: "swift-composable-architecture"
-                )
+                ),
+
             ]
         ),
         .testTarget(
             name: "RaelizeInputMethodKitTests",
             dependencies: [
                 "RaelizeInputMethodKit",
+                "RaelizeLogic",
                 .product(name: "Testing", package: "swift-testing"),
             ]),
     ]
