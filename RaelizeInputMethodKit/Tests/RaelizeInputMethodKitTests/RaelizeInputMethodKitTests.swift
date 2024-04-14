0c8b1b0576ca63c4a9d8229f322f2b3e2a108cfe
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-04-15 00:38:40 +0900

    Add RaelizeInputMethodKit resetState test

--- RaelizeInputMethodKit/Tests/RaelizeInputMethodKitTests/RaelizeInputMethodKitTests.swift
+++ RaelizeInputMethodKit/Tests/RaelizeInputMethodKitTests/RaelizeInputMethodKitTests.swift
@@ -5,6 +5,9 @@
 //
 //
 
+import AppKit
+import ComposableArchitecture
+import RaelizeLogic
 import Testing
 
 @testable import RaelizeInputMethodKit
@@ -13,7 +16,28 @@ struct RaelizeInputMethodKitTests {
 
     private let example = true
 
-    @Test func sample() {
-        #expect(self.example == true)
+    @Suite("RaelizeInputMethodKit.Action Tests")
+    final class Action {
+
+        @Test @MainActor
+        func resetState() async {
+            let store = TestStore(
+                initialState: RaelizeIMKReducer.State(raelizeState: .inputMode),
+                reducer: { RaelizeIMKReducer() },
+                withDependencies: {
+                    $0.wordListFileUseCase = UseCaseProvider.shared.wordListFileUseCase
+                })
+            await store.send(.resetState(.neutralMode)) {
+                $0.raelizeState = .neutralMode
+            }
+        }
+        //
+        //        @Test
+        //        func operationEventKey() async {
+        //            let store = TestStore(initialState: RaelizeIMKReducer.State(raelizeState: .neutralMode) , reducer: { RaelizeIMKReducer() }, withDependencies: { $0.wordListFileUseCase.resetWordList() })
+        //            var currentState = RaelizeIMKReducer.State(raelizeState: .neutralMode)
+        //            var enterKeyEvent = NSEvent.keyEvent(with: .keyDown, location: NSPoint(), modifierFlags: NSEvent.ModifierFlags(), timestamp: 0, windowNumber: 0, context: nil, characters: "", charactersIgnoringModifiers: "", isARepeat: false, keyCode: 36)!
+        //
+        //        }
     }
 }
